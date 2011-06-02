5ee1e7c469b57b4ff870463e244b4ee94de95dbf
Marc Peabody <marcpeabody@yahoo.com>
2011-06-02 11:08:43 -0700

    Some personal Ruby style preferences applied. Cool script. Thanks!

--- quote.rb
+++ quote.rb
@@ -1,27 +1,15 @@
-require 'rss/1.0'
 require 'rss/2.0'
 require 'open-uri'
 require 'colorize'
 
-#Defines Function for word wraping
-def wrap_text(txt, col = 80)
-  txt.gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/, "\\1\\3\n") 
+@color = (String.colors[1..7] + String.colors[10..15]).sample # excluding 8 white and 9 default
+@col   = ARGV.last || 50
+
+def print_quote(quote, author)
+  puts "#{quote}\n\t\t-#{author}".gsub(/(.{1,#{@col}})( +|$\n?)|(.{1,#{@col}})/, "\\1\\3\n").colorize(@color)
 end
 
-#Obtain rss feed of randome quote
 source = "http://www.quotedb.com/quote/quote.php?action=random_quote_rss&=&=&" # url or local file
-content = "" # raw content of rss feed will be loaded here
-open(source) do |s| content = s.read end
-rss = RSS::Parser.parse(content, false)
+rss = RSS::Parser.parse(open(source), false)
 
-#Pick a randome color to output with
-posible_colors = [1,2,3,4,5,6,10,11,12,13,14,15]
-color = String.colors[posible_colors[rand(posible_colors.size)]]
-
-#Extract Data fromat and output...Any questions
-quote = wrap_text(rss.channel.item.description.colorize(color), 50)
-author = wrap_text("\n\t\t-#{rss.channel.item.title}".colorize(color), 50)
-
-#Print the lines
-puts quote
-puts author
+print_quote(rss.channel.item.description, rss.channel.item.title)
